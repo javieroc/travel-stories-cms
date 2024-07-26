ec26e4834562963728701b3989498054719ccd9b
Javier Ocampo <ferroxido@gmail.com>
2024-07-26 17:01:37 +0100

    Setup project :rocket:

---
+++ src/api/travel-story/controllers/travel-story.ts
@@ -0,0 +1,7 @@
+/**
+ * travel-story controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::travel-story.travel-story');
---
+++ src/api/travel-story/routes/travel-story.ts
@@ -0,0 +1,7 @@
+/**
+ * travel-story router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::travel-story.travel-story');
